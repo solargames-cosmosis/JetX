aa4a0e74516be1888c56b8cfa0cb9275b3a21adf
Solar - Vapor Offical <help.solargames@gmail.com>
2026-05-04 20:42:32 -0400

    Create theme.js

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+document.documentElement.style.setProperty('--bg-color', '#0a0a0a');
+document.documentElement.style.setProperty('--text-color', '#ffffff');
+document.documentElement.style.setProperty('--accent', '#ff0033');
+
+document.body.style.background = 'var(--bg-color)';
+document.body.style.color = 'var(--text-color)';
